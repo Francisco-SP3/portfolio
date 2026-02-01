31c8bd6e9fac7e7b0abcb26809da710e7ddbd9cb
Francisco-SP3 <paco.salas.porras@gmail.com>
2026-02-01 16:57:38 -0600

    ops(github): deployment in github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-import edgeoneAdapter from '@edgeone/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: edgeoneAdapter(),
+  site: 'https://Francisco-SP3.github.io',
+  base: '/portfolio',
   output: 'static',
   integrations: [react()]
 });
