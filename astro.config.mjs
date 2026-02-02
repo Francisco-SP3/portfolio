717c21e8d4d34d444c96959c89ffedb4dc17db56
Francisco-SP3 <paco.salas.porras@gmail.com>
2026-02-01 18:04:46 -0600

    fix: remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Francisco-SP3.github.io',
-  base: '/portfolio',
   output: 'static',
   integrations: [react()]
 });
